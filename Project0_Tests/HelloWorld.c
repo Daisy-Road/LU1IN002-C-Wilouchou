882e2db6672616337ab8414c2071c60f558e94cc
Supre-le-sucre <wilhemblondel@gmail.com>
2023-01-18 18:51:29 +0100

    Nique ta mère

--- Project0_Tests/HelloWorld.c
+++ Project0_Tests/HelloWorld.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main() 
-{
-    int test = 5;
-    test ++;
-    printf("\nHello World %d!\n", test);
-    printf("Testing NeoVim capabilities with git and writing code !\n");
-    test--;
-    return 0;
+int main() {
+  int test = 5;
+  test++;
+  printf("\nHello World %d!\n", test);
+  printf("Testing NeoVim capabilities with git and writing code !\n");
+  test--;
+  return 0;
 }
