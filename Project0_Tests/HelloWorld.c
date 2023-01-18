b929b90a8e3570b71bc70a66d2c2321ba5cc6d8b
Supre-le-sucre <wilhemblondel@gmail.com>
2023-01-18 11:23:39 +0100

    Doing everything with AstroNvim

--- Project0_Tests/HelloWorld.c
+++ Project0_Tests/HelloWorld.c
@@ -5,6 +5,7 @@ int main()
     int test = 5;
     test ++;
     printf("\nHello World %d!\n", test);
+    printf("Testing NeoVim capabilities with git and writing code !\n");
     test--;
     return 0;
 }
