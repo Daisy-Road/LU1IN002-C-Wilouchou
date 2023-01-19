1a31cf434d93e0668335a608d74c80411d49da4d
Wilhem Blondel <Supre-le-sucre>
2023-01-19 12:05:13 +0100

    Added new FoncAff to learn about format and prototype

--- Project0_Tests/HelloWorld.c
+++ Project0_Tests/HelloWorld.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+/* 
+Format:
+%d decimal
+%s string
+%c character
+%f float
+*/
 
-int main() 
-{
+int FoncAff(const char* aff, int val); // Prototype
+
+int main() {
     int test = 5;
     test++;
-    int out = printf("Hello World %d!\n", test);
+    char* hw = "Hello World";
+    int out = FoncAff(hw, test);
     printf("Value of out is %d\n", out);
-    printf("Testing NeoVim capabilities with git and writing code !\n");
     test--;
     return 0;
+}
+
+int FoncAff(const char* aff, int val) {
+    //print out the string
+    return printf("%s\nValue is %d\n", aff, val);
 }
