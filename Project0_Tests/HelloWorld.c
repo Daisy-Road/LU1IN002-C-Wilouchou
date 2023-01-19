7c10d4bfeb2f923e4fd99bdbde9a5c807b3828a8
Wilhem Blondel <Supre-le-sucre>
2023-01-19 11:42:36 +0100

    Added new var to test out printf

--- Project0_Tests/HelloWorld.c
+++ Project0_Tests/HelloWorld.c
@@ -5,7 +5,8 @@ int main()
 {
     int test = 5;
     test++;
-    printf("\nHello World %d!\n", test);
+    int out = printf("Hello World %d!\n", test);
+    printf("Value of out is %d\n", out);
     printf("Testing NeoVim capabilities with git and writing code !\n");
     test--;
     return 0;
