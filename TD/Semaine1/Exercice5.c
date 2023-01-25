cabad20f56ddc3ec24ce2c1fc9ba16e526ccaed8
Supre-le-sucre <wilhemblondel@gmail.com>
2023-01-25 22:33:57 +0100

    Tifenn said it was moche

--- TD/Semaine1/Exercice5.c
+++ TD/Semaine1/Exercice5.c
@@ -9,36 +9,55 @@
 float prixPlace(const int age, const int jour, const float heure);
 
 int main() {
-    int age = 24; int jour = 6; float heure = 9.5;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
-    age = 6; jour = 3; heure = 16.7;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
-    age = 24; jour = 2; heure = 9.3;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
-    age = 68; jour = 6; heure = 20.6;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
-    age = 23; jour = 7; heure = 18.5;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
-    age = 14; jour = 6; heure = 10.5;
-    printf("Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n", age, jour, heure, prixPlace(age, jour, heure));
+    int age = 24;
+    int jour = 6;
+    float heure = 9.5;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
+    age = 6;
+    jour = 3;
+    heure = 16.7;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
+    age = 24;
+    jour = 2;
+    heure = 9.3;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
+    age = 68;
+    jour = 6;
+    heure = 20.6;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
+    age = 23;
+    jour = 7;
+    heure = 18.5;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
+    age = 14;
+    jour = 6;
+    heure = 10.5;
+    printf(
+        "Age: %d | Jour: %d | Heure Séance: %.1f\nPrix + avantageux: %.2f€\n\n",
+        age, jour, heure, prixPlace(age, jour, heure));
     return 0;
 }
 
 float prixPlace(const int age, const int jour, const float heure) {
-    if(age < 14) {
+    if (age < 14) {
         return TMOINS14;
-    }
-    else if(age < 26 && jour <= 5) {
+    } else if (age < 26 && jour <= 5) {
         return TMOINS26S;
-    }
-    else if(heure >= 8 && heure <= 11) {
-        return TMATIN;   
-    }
-    else if(age < 26 && jour > 5) {
+    } else if (heure >= 8 && heure <= 11) {
+        return TMATIN;
+    } else if (age < 26 && jour > 5) {
         return TMOINS26WE;
-    }
-    else {
+    } else {
         return TNORMAL;
     }
-
 }
