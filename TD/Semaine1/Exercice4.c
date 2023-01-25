bbcd45dc8e71a5783171313d2783f85b62ef34ee
Supre-le-sucre <wilhemblondel@gmail.com>
2023-01-25 21:32:10 +0100

    Fixed Exercice4

--- TD/Semaine1/Exercice4.c
+++ TD/Semaine1/Exercice4.c
@@ -6,16 +6,20 @@ int main() {
     assert(signeSomme(-1, -1) == -1);
     assert(signeSomme(-1, 1) == 0);
     assert(signeSomme(1, -1) == 0);
-    assert(signeSomme(-6, 8) == 0);
+    assert(signeSomme(-6, 8) == 1);
     assert(signeSomme(6, -8) == -1);
     assert(signeSomme(-12, 2) == -1);
+    assert(signeSomme(12, -2) == 1);
+    assert(signeSomme(0, 0) == 0);
     return 0;
 }
 
 int signeSomme(const int a, const int b) {
-    if((a < 0 && -a > b) || (b < 0 && -b > a)) {
+    if ((a < 0 && -a > b) || (b < 0 && -b > a)) {
         return -1;
-    } else {
+    } else if (a == -b) {
         return 0;
+    } else {
+        return 1;
     }
 }
