e05b2e86b5bd9190c773f740e9de1544719b508c
Supre-le-sucre <wilhemblondel@gmail.com>
2023-01-26 16:36:17 +0100

    Corrected Exercice2.c

--- TD/Semaine1/Exercice2.c
+++ TD/Semaine1/Exercice2.c
@@ -5,8 +5,9 @@ float multi(int k, float x);
 int main() {
     int op1 = 3;
     float op2 = 2.7;
+    float result = multi(op1, op2);
 
-    printf("%d * %.2f = %.2f\n", op1, op2, multi(op1, op2));
+    printf("%d * %.2f = %.2f\n", op1, op2, result);
 
     return 0;
 }
