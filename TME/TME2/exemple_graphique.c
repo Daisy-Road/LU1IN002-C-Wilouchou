f956c91e725045ee82fe3d80a041efb8716b894f
Supre-le-sucre <wilhemblondel@gmail.com>
2023-02-02 19:51:33 +0100

    .

--- TME/TME2/exemple_graphique.c
+++ TME/TME2/exemple_graphique.c
@@ -2,7 +2,7 @@
  
 int main() {
  
-  CINI_open_window(400, 400, "test");
+  CINI_open_window(400, 300, "test");
   
   CINI_draw_pixel(199, 200, "white");   /* affiche pt coord x=199, y=200*/
   CINI_draw_pixel(200, 200, "white");   /* affiche pt coord x=200, y=200*/
