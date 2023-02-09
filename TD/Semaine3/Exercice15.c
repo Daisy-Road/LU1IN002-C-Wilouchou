c16cb991c1faf9b6de970aa211dd989a5fa0bbe4
Supre-le-sucre <wilhemblondel@gmail.com>
2023-02-09 17:25:57 +0100

    Fixed Exercice15.c

--- TD/Semaine3/Exercice15.c
+++ TD/Semaine3/Exercice15.c
@@ -1,4 +1,4 @@
-#include <assert.h>
+#include <stdio.h>
 
 void permute(int* a, int* b);
 void permute_circulaire(int* a, int* b, int* c);
@@ -6,6 +6,9 @@ void permute_circulaire(int* a, int* b, int* c);
 int main() {
     int a, b, c;
     a=10; b=20; c=30;
+    printf("a=%d, b=%d, c=%d\n", a, b, c);
+    permute_circulaire(&a, &b, &c);
+    printf("a=%d, b=%d, c=%d\n", a, b, c);
     return 0;
 }
 
@@ -16,7 +19,6 @@ void permute(int* a, int* b) {
 }
 
 void permute_circulaire(int* a, int* b, int* c) {
-    int tmp = *a;
     permute(a, b);
-    permute(&tmp, c);
+    permute(b, c);
 }
