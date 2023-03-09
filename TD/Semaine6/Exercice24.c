48be87b76851261ef3c26803f869e0dfc7e756b0
Supre-le-sucre <wilhemblondel@gmail.com>
2023-03-09 16:33:04 +0100

    TD

--- TD/Semaine6/Exercice24.c
+++ TD/Semaine6/Exercice24.c
@@ -4,6 +4,7 @@ int equals(char* s1, char* s2) {
     int i = 0;
     while (s1[i] != '\0') {
         if (s1[i] != s2[i]) return 0;
+        i++;
     }
     return s2[i] == '\0';
 }
@@ -17,4 +18,15 @@ int equals_recu(char* s1, char* s2) {
     }
 }
 
-int main() { return 0; }
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Usage: ./Exercice24.out <string1> <string2>\n");
+        return 1;
+    }
+    char* s1 = argv[1];
+    char* s2 = argv[2];
+    printf("equals(\"%s\", \"%s\") returned %d\n", s1, s2, equals(s1, s2));
+    printf("equals_recu(\"%s\", \"%s\") returned %d\n", s1, s2,
+           equals_recu(s1, s2));
+    return 0;
+}
--- TD/Semaine6/Exercice25.c
+++ TD/Semaine6/Exercice25.c
@@ -1,1 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TIMEOUT 10
+
+void init_rand_ord_tab(int* tab, int len) {
+    int min = 0;
+    int max = 5;
+    for (int i = 0; i < len; i++) {
+        tab[i] = (rand() % (max - min + 1)) + min;
+        max += 5;
+        min += 5;
+    }
+}
+
+void affiche(int* tab, int len) {
+    printf("tab = {");
+    for (int i = 0; i < len - 1; i++) {
+        printf("%d, ", tab[i]);
+    }
+    printf("%d}\n", tab[len - 1]);
+}
+
+int recherche(int* tab, int len, int num) {
+    int min = 0;
+    int max = len;
+    int i = len / 2;
+    while (min <= max) {
+        if (tab[i] == num) return 1;
+        if (tab[i] < num) {
+            min = i + 1;
+        } else {
+            max = i - 1;
+        }
+        i = (max + min) / 2;
+    }
+    return tab[i] == num;
+}
+
+int recherche_recu(int* tab, int len, int num) {
+    int i = len / 2;
+    if (len == 0) return 0;
+    if (tab[i] == num) return 1;
+    if (tab[i] < num) return recherche_recu(tab + len / 2 + 1, len / 2, num);
+    else return recherche_recu(tab, len / 2 - 1, num);
+}
+
+int main() {
+    srand(time(NULL));
+    int tab[10];
+    int search;
+    init_rand_ord_tab(tab, 10);
+    affiche(tab, 10);
+    printf("What are we looking for ? ");
+    scanf("%d", &search);
+    printf("\n");
+    printf("%d\n", recherche_recu(tab, 10, search));
+
+    return 0;
+}
